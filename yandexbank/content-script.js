2397fb59b60bc11ae9e0281b4331160fadebc5a3
kluchko <kluchko@lers.ru>
2019-02-20 15:17:28 +1000

    feat: add extension for yandex bank

---
+++ yandexbank/content-script.js
@@ -0,0 +1,155 @@
+console.log("content-script loaded")
+
+// Listen for messages from browser-action or background script
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    var response;
+
+    // Message to check whether this script has been injected
+    if (request === "ping") {
+        // "Yeah, we're good"
+        response = "pong";
+    }
+    else if (request == "export") {
+        try {
+            exportTransactionsCommandHandler();
+            console.log("Export completed");
+            response = "ok";
+        } catch (error) {
+            console.log("Export error: " + error);
+            response = "error";
+        }
+    }
+
+    sendResponse(response);
+});
+
+function exportTransactionsCommandHandler() {
+    let token = getToken();
+    if (!token) {
+        alert("Error: Token is " + token);
+        throw Error("Error: Token is " + token);
+    }
+
+    let accountCode = getAccountCode();
+    if (!accountCode) {
+        alert("Error: accountCode is " + accountCode);
+        throw Error("Error: accountCode is " + accountCode);
+    }
+
+    prepareToParse();
+    let transactions = parseTransactions();
+    let balance = parseBalance();
+
+    if (token && transactions && transactions.length)
+        sendTransactions(token, accountCode, { balance, transactions });
+}
+
+function getToken() {
+    var auth = localStorage.getItem("auth");
+
+    try {
+        var jsonAuth = auth && JSON.parse(auth);
+    }
+    catch{ }
+
+    return jsonAuth && jsonAuth.token;
+}
+
+function getAccountCode() {
+    return localStorage.getItem("accountCode");
+}
+
+function parseDate(text) {
+    const months = { "янв": 0, "фев": 1, "мар": 2, "апр": 3, "май": 4, "июн": 5, "июл": 6, "авг": 7, "сен": 8, "окт": 9, "ноя": 10, "дек": 11 };
+
+    let matches = text.replace(/[+\s]/g, " ").match(/(\d{1,2}) (янв|фев|мар|апр|май|июн|июл|авг|сен|окт|ноя|дек) (\d{4}|\d{0})/);
+    if (matches) {
+        let year = new Date().getFullYear();
+        if (matches[3])
+            year = +matches[3];
+        let date = new Date(Date.UTC(year, months[matches[2]], +matches[1]));
+        return date;
+    }
+
+    matches = text.match(/(Сегодня|Вчера)/);
+    if (matches) {
+        if (matches[1] === 'Сегодня') {
+            let nowDate = new Date();
+            let date = new Date(Date.UTC(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate()));
+            return date;
+        }
+        if (matches[1] === 'Вчера') {
+            let nowDate = new Date();
+            let date = new Date(Date.UTC(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate()));
+            date.setDate(date.getDate() - 1);
+            return date;
+        }
+    }
+}
+
+function parseBalance() {
+    return document.querySelector(".balance-widget__amount .price__amount").pipe(setBorder, innerText, parseAmount);
+}
+
+function parseAmount(text) {
+    return parseFloat(text.replace(/[+\s]/g, "").replace(/,/g, ".").replace(/–/g, "-"));
+}
+
+function firstChildText(element) {
+    if (element.firstChild && element.firstChild.nodeType == 3) {
+        return element.firstChild.textContent.trim();
+    }
+}
+
+function innerText(element) {
+    return element.innerText.trim();
+}
+
+function setBorder(element) {
+    let border = "1px solid red";
+    element.style.border = border;
+    return element;
+}
+
+function prepareToParse() {
+    const _pipe = (a, b) => (arg) => b(a(arg));
+    const pipe = (...ops) => ops.reduce(_pipe);
+
+    Object.defineProperty(Element.prototype, "pipe", {
+        value: function (...ops) {
+            return pipe(...ops)(this);
+        },
+        writable: true,
+        configurable: true
+    });
+}
+
+function parseTransactions() {
+    let transactions = [];
+    let records = document.querySelectorAll(".history-item-list .payment-history-item");
+    for (let record of records) {
+        let dateTime = record.querySelector(".payment-history-item__date").pipe(setBorder, innerText, parseDate);
+        let description = record.querySelector(".payment-history-item__text").pipe(innerText);
+        let amount = record.querySelector(".payment-history-item__sum").pipe(innerText, parseAmount);
+
+        let transaction = { description, dateTime, amount };
+        console.log(JSON.stringify(transaction));
+        transactions.push(transaction);
+    }
+
+    return transactions;
+}
+
+function sendTransactions(token, accountCode, data) {
+    var req = new XMLHttpRequest();
+    req.open('PATCH', 'https://myfinance-server.herokuapp.com/api/v1/accounts/' + accountCode, true);
+    req.setRequestHeader("Content-Type", "application/json");
+    req.setRequestHeader("Authorization", "Bearer " + token);
+    req.onreadystatechange = function () {
+        if (req.readyState == 4 && req.status == 204) {
+            alert("Successful!");
+        }
+    };
+
+    req.send(JSON.stringify(data));
+}
